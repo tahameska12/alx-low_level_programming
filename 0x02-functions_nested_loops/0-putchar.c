60d88021b536252e0378f967022b6104e4454441
tahameska12 <meska796@example.com>
2023-08-15 09:10:47 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
 #include "main.h"
-
-int main(void){
-  char str[] = "_putchar";
-  int ch;
-
-  for (ch = 0; ch < 8; ch++)
-	  _putchar(str[ch]);
-  _putchar("\n");
-
-  return(0);
+/**
+ * main - Entry point
+ * Description: 'prints _putcher to stdout'
+ * Return: always 0
+ */
+int main(void)
+{
+        putchar('');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }
