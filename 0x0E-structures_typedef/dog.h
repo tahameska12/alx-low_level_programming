6ba6386654d51ee62016e0e846952edaa77748d2
tahameska12 <meska796@example.com>
2023-09-11 19:34:11 -0700

    init dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
- * struct dog - a dog's basic info
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- */
-
-struct dog{
-
-	char *name;
-	float age;
-	char *owner;
-};
-
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
